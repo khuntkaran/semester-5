38dc30238b5f1efe9af0901ba1a7298bb6ce1a22
khuntkaran <94033442+khuntkaran@users.noreply.github.com>
2023-06-27 13:43:05 +0530

    Add files via upload

---
+++ .Net/wasteg/02_distance_constractor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Enter Distance 1 : ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Distance 2 : ");
+            int b=Convert.ToInt32(Console.ReadLine());
+
+            Distance c1 = new Distance(a, b);
+            c1.Calculate();
+
+
+                Console.ReadLine();
+        }
+    }
+
+    class Distance
+    {
+        public int dis1, dis2;
+
+        public Distance(int a, int b)
+        {
+            this.dis1 = a;
+            this.dis2 = b;
+        }
+
+        public void Calculate()
+        {
+            Console.WriteLine("Distance = " + (dis1 + dis2));
+        }
+    }
+}
