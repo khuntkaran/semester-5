b92ef8a43a56260e99cf7d32ba6a215363c5b9f3
khuntkaran <khuntkaran995@gmail.com>
2023-07-04 18:43:32 +0530

    .

--- .Net/03_oop/04_Furniture_interface.cs
+++ .Net/03_oop/04_Furniture_interface.cs
@@ -4,54 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-    interface Furniture2
+interface Furniture2
+{
+    int Price { get; set; }
+
+    void Calculate();
+}
+
+class Table2 : Furniture2
+{
+    public int price;
+    public int Price
     {
-        int Price { get; set; }
-       
-        void Calculate();
+        get
+        {
+            return price;
+        }
+        set
+        {
+            price = value;
+        }
     }
 
-    class Table2 : Furniture2
+    String Material;
+    public int Hight, SurfacArea;
+
+
+    public void setData()
     {
-        public int price;
-        public int Price
-        {
-            get
-            {
-                return price;
-            }
-            set
-            {
-                price = value;
-            }
-        }
-      
-        String Material;
-        public int Hight, SurfacArea;
-
-
-        public void setData()
-        {
-            Console.WriteLine("Enter Price : ");
-            Price = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Material : ");
-            Material = Console.ReadLine();
-            Console.WriteLine("Enter Hight : ");
-            Hight = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter SurfacArea : ");
-            SurfacArea = Convert.ToInt32(Console.ReadLine());
-        }
-        public void getData()
-        {
-            Console.WriteLine("Price : " + Price);
-            Console.WriteLine("Material : " + Material);
-            Console.WriteLine("Hight : " + Hight);
-            Console.WriteLine("SurfacArea : " + SurfacArea);
-        }
-
-        public void Calculate()
-        {
-            Console.WriteLine("Cost : " + (Hight * Price));
-        }
+        Console.WriteLine("Enter Price : ");
+        Price = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Material : ");
+        Material = Console.ReadLine();
+        Console.WriteLine("Enter Hight : ");
+        Hight = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter SurfacArea : ");
+        SurfacArea = Convert.ToInt32(Console.ReadLine());
     }
-    
+    public void getData()
+    {
+        Console.WriteLine("Price : " + Price);
+        Console.WriteLine("Material : " + Material);
+        Console.WriteLine("Hight : " + Hight);
+        Console.WriteLine("SurfacArea : " + SurfacArea);
+    }
+
+    public void Calculate()
+    {
+        Console.WriteLine("Cost : " + (Hight * Price));
+    }
+}
+
