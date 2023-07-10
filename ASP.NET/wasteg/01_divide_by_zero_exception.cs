edb0ae1ea05a835755adb25e97a2494e8e1be64d
khuntkaran <94033442+khuntkaran@users.noreply.github.com>
2023-07-10 13:40:04 +0530

    Add files via upload

---
+++ ASP.NET/wasteg/01_divide_by_zero_exception.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_exception
+{
+    class _01_divide_by_zero_exception
+    {
+        public void check()
+        {
+            try
+            {
+                Console.Write("Enter NO : ");
+                int i = Convert.ToInt32(Console.ReadLine());
+                float a = 20 / i;
+                Console.WriteLine("Ans : " + a);
+            }
+            catch (Exception e)
+            {
+               Console.WriteLine("Exception : "+e.Message);
+            }
+        }
+    }
+}
---
+++ ASP.NET/wasteg/02_IndexOutOfRange_Exception.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_exception
+{
+    class IndexOutOfRange_Exception
+    {
+        public void readArray()
+        {
+            int[] a = new int[4];
+            try
+            {
+                for (int i = 0; i <= a.Length; i++)
+                {
+                    Console.Write("Enter " + (i + 1) + " Element : ");
+                    a[i] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception : " + e.Message);
+            }
+                
+        }
+    }
+}
